326852e030f6b31e966c495061047bb7cafd7721
Russ93 <russ93@fullsail.edu>
2014-07-28 14:42:57 -0400

    new github

--- lab/day3/www-root/js/app.js
+++ lab/day3/www-root/js/app.js
@@ -1,7 +1,7 @@
 (function() {
   var App, URL;
 
-  App = angular.module('connecting_point', ['ngRoute', 'firebase']);
+  App = angular.module('connecting_point', ['ngRoute', 'firebase', 'ngResource', 'base64']);
 
   URL = "https://vivid-fire-5420.firebaseio.com";
 
@@ -17,8 +17,47 @@
     }
   ]);
 
+  App.factory("AngularGit", function($resource) {
+    return $resource("https://api.github.com/repos/angular/angular.js/:category/:sha");
+  });
+
+  App.factory("GetRepos", function($resource) {
+    return $resource("https://api.github.com/users/:username/repos", null, {
+      'get': {
+        method: 'get',
+        isArray: true
+      }
+    });
+  });
+
+  App.factory("GetCommits", function($resource) {
+    return $resource("https://api.github.com/repos/:owner/:repo/commits", null, {
+      'get': {
+        method: 'get',
+        isArray: true
+      }
+    });
+  });
+
+  App.factory("GitTrees", function($resource) {
+    return $resource("https://api.github.com/repos/:owner/:repo/git/trees/:sha?recursive=1");
+  });
+
+  App.factory("GetContents", function($resource) {
+    return $resource("https://api.github.com/repos/:owner/:repo/contents/:path", null, {
+      'get': {
+        method: 'get',
+        isArray: true
+      }
+    });
+  });
+
+  App.factory("GetFile", function($resource) {
+    return $resource("https://api.github.com/repos/:owner/:repo/contents/:path");
+  });
+
   App.run([
-    "$firebaseSimpleLogin", "$rootScope", "$location", function($firebaseSimpleLogin, $rootScope, $location) {
+    "$firebaseSimpleLogin", "$rootScope", "$location", "GetRepos", function($firebaseSimpleLogin, $rootScope, $location, GetRepos) {
       var conn;
       $rootScope.close = function() {
         return $rootScope.modal = false;
@@ -26,14 +65,10 @@
       URL = "https://vivid-fire-5420.firebaseio.com";
       conn = new Firebase(URL);
       $rootScope.loginObj = $firebaseSimpleLogin(conn);
-      $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
-        return $location.path('/user');
-      });
-      $rootScope.$on("$firebaseSimpleLogin:logout", function(e) {
+      return $rootScope.$on("$firebaseSimpleLogin:logout", function(e) {
         console.log("logout");
-        return $location.path('/');
+        $location.path('/');
       });
-      return console.log("$rootScope.loginObj", $rootScope.loginObj.user);
     }
   ]);
 
@@ -43,8 +78,8 @@
       if (!(obj instanceof Object)) {
         return obj;
       }
-      return Object.keys(obj).filter(function(key) {
-        if (key.charAt(0) !== "$") {
+      return Object.keys(obj.filter(function(key) {
+        if (key.charAt(0 !== "$")) {
           return key;
         }
       }).map(function(key) {
@@ -52,7 +87,7 @@
           __proto__: null,
           value: key
         });
-      });
+      }));
     };
   });
 
@@ -63,20 +98,110 @@
   ]);
 
   App.controller("UserCtrl", [
-    "$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase) {
+    "$scope", "$rootScope", "$firebase", "GetRepos", "GetCommits", "GetContents", "GetFile", "$base64", function($scope, $rootScope, $firebase, GetRepos, GetCommits, GetContents, GetFile, $base64) {
+      var secret, trim;
+      $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+        var params;
+        console.log(user.username);
+        params = {
+          username: user.username
+        };
+        $scope.repos = GetRepos.get(params);
+        return console.log($scope.repos);
+      });
       $scope.title = "User - loginObj.user";
       $scope.conn = $firebase(new Firebase(URL + "/name"));
+      secret = "c2e8b102ca576e0060f6dc493956288170d6eaaa";
+      trim = function(x) {
+        return x.replace(/^\s+|\s+$/gm, '');
+      };
+      $scope.openItem = function($event) {
+        var params;
+        if ($event.target.children[0] === void 0) {
+          params = {
+            owner: $rootScope.loginObj.user.username,
+            repo: $scope.repos[$scope.repo].name,
+            path: trim($event.target.textContent)
+          };
+          return GetFile.get(params);
+        }
+      };
       $scope["new"] = function() {
         return $rootScope.modal = true;
       };
-      return $scope.create = function() {
+      $scope.create = function() {
         $rootScope.modal = false;
-        $scope.conn.$child("files").$add({
+        $scope.conn.$child("files".$add({
           name: $scope.newFile
-        });
+        }));
         return console.log({
           file: $scope.newFile
         });
+      };
+      $scope.$watch('sha', function(newValue, oldValue) {
+        var params;
+        if ($rootScope.loginObj.user !== null) {
+          params = {
+            owner: $rootScope.loginObj.user.username,
+            repo: $scope.repos[$scope.repo].name
+          };
+          $scope.repoContents = GetContents.get(params);
+          return console.log($scope.repoContents);
+        }
+      });
+      $scope.$watch('repo', function(newValue, oldValue) {
+        var params;
+        if ($rootScope.loginObj.user !== null) {
+          params = {
+            owner: $rootScope.loginObj.user.username,
+            repo: $scope.repos[$scope.repo].name
+          };
+          return GetCommits.get(params, function(data) {
+            return $scope.sha = data[data.length - 1].sha;
+          });
+        }
+      });
+    }
+  ]);
+
+  App.directive("folder", [
+    function() {
+      return {
+        restrict: "AE",
+        template: '<li ng-repeat="cont in contents" file="cont" repo="repo"></li>',
+        scope: {
+          folder: "=",
+          repo: '='
+        },
+        controller: function($rootScope, $scope, GetContents) {
+          var params;
+          console.log($scope.folder);
+          params = {
+            owner: $rootScope.loginObj.user.username,
+            repo: $scope.repo,
+            path: $scope.folder
+          };
+          return $scope.contents = GetContents.get(params);
+        }
+      };
+    }
+  ]);
+
+  App.directive("file", [
+    '$compile', function($compile) {
+      return {
+        restrict: "AE",
+        template: '{{file.name}}',
+        scope: {
+          file: "=",
+          repo: "="
+        },
+        link: function($scope, $element, attrs) {
+          if ($scope.file.type === 'dir') {
+            $element.append('<ul folder="file.path" repo="repo"></ul>');
+            return $compile($element.contents())($scope);
+          }
+        }
       };
     }
   ]);
--- lecture/day 4/www-root/js/app.js
+++ lecture/day 4/www-root/js/app.js
@@ -1,55 +1,78 @@
-var App = angular.module('day4app', ['ngRoute', 'firebase']);
-var myDataRef = new Firebase('https://q4fxwkikfk9.firebaseio-demo.com/');
+(function() {
+  var App, URL;
 
-stuff = {
-	"step1": "myDataRef.set('User ' + name + ' says ' + text)",
-	"step2": "myDataRef.set({name: name, text: text})",
-	"step3": "myDataRef.push({name: name, text: text})"
-}
+  App = angular.module("day4app", ['ngRoute', 'firebase']);
 
-App.config(['$routeProvider', function($routeProvider){
-	$routeProvider
-		.when('/',{
-			templateUrl:'views/home.html',
-			controller: 'HomeCtrl'
-		})
-		.when('/detail',{
-			templateUrl:'views/detail.html',
-			controller: 'DetailCtrl'
-		})
-}])
+  URL = "https://lecture.firebaseio.com/";
 
-App.filter('toArray', function () {
-    'use strict';
- 
-    return function (obj) {
-        if (!(obj instanceof Object)) {
-            return obj;
+  App.config([
+    "$routeProvider", function($routeProvider) {
+      return $routeProvider.when("/", {
+        templateUrl: "views/home.html",
+        controller: "HomeCtrl"
+      }).when("/detail/:id", {
+        templateUrl: "views/detail.html",
+        controller: "DetailCtrl"
+      });
+    }
+  ]);
+
+  App.filter("toArray", function() {
+    "use strict";
+    return function(obj) {
+      if (!(obj instanceof Object)) {
+        return obj;
+      }
+      return Object.keys(obj).filter(function(key) {
+        if (key.charAt(0) !== "$") {
+          return key;
         }
- 
-        return Object.keys(obj).filter(function(key){if(key.charAt(0) !== "$") {return key;}}).map(function (key) {
-            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+      }).map(function(key) {
+        return Object.defineProperty(obj[key], "$key", {
+          __proto__: null,
+          value: key
         });
+      });
     };
-});
+  });
 
-App.controller('HomeCtrl', ['$scope', "$rootScope", "$firebase",function ($scope, $rootScope, $firebase){
-	var URL;
-	$scope.title = "Home";
-	$rootScope.pageTitle = "Home";
-	URL = "https://vivid-fire-5420.firebaseio.com/";
-	$scope.items = $firebase(new Firebase(URL));
-	// $scope.items.$set($scope.posts);
-	$scope.savePost = function(){
-		$scope.items.$add($scope.newPost);
-		$scope.newPost = {};
-	}
-}]);
+  App.controller("HomeCtrl", [
+    "$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase) {
+      $scope.items = $firebase(new Firebase(URL));
+      $scope.title = "Home";
+      return $scope.savePost = function() {
+        $scope.items.$add($scope.newPost);
+        return $scope.newPost = {};
+      };
+    }
+  ]);
 
-App.controller('DetailCtrl', ['$scope', "$rootScope", function ($scope, $rootScope){
-	$scope.title = "Detail";
-	$rootScope.pageTitle = "Detail";
-	$scope.clickMe = function(){
-		alert('OOOOHHHH NOOOOOES!!');
-	}
-}])
+  App.controller("DetailCtrl", [
+    "$scope", "$rootScope", "$routeParams", "$firebase", function($scope, $rootScope, $routeParams, $firebase) {
+      $scope.title = "Detail";
+      $rootScope.pageTitle = "Detail Page";
+      return $scope.post = $firebase(new Firebase(URL + $routeParams.id));
+    }
+  ]);
+
+  App.directive("commentsSection", [
+    function() {
+      return {
+        restrict: "AE",
+        templateUrl: "views/comments.html",
+        scope: {
+          postId: '@commentSection'
+        },
+        controller: function($scope, $firebase) {
+          $scope.comments = $firebase(new Firebase(URL + $scope.postId + "/comments"));
+          console.log($scope.comments);
+          $scope.saveComment = function() {
+            $scope.comments.$add($scope.newComment);
+            $scope.newComment = {};
+          };
+        }
+      };
+    }
+  ]);
+
+}).call(this);
