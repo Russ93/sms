907244c590bb9f6e06a20f1a97d918b369615d26
Russ93 <russ93@fullsail.edu>
2014-07-30 01:30:48 -0400

    added ACE

--- lab/day3/www-root/js/app.js
+++ lab/day3/www-root/js/app.js
@@ -1,7 +1,7 @@
 (function() {
   var App, URL;
 
-  App = angular.module('connecting_point', ['ngRoute', 'firebase', 'ngResource', 'base64']);
+  App = angular.module('connecting_point', ['ngRoute', 'firebase', 'ngResource', 'ui.ace']);
 
   URL = "https://vivid-fire-5420.firebaseio.com";
 
@@ -91,40 +91,162 @@
     };
   });
 
+  App.filter("base64", function() {
+    return function(string) {
+      var Base64;
+      Base64 = {
+        _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        encode: function(e) {
+          var a, f, i, n, o, r, s, t, u;
+          t = "";
+          n = void 0;
+          r = void 0;
+          i = void 0;
+          s = void 0;
+          o = void 0;
+          u = void 0;
+          a = void 0;
+          f = 0;
+          e = Base64._utf8_encode(e);
+          while (f < e.length) {
+            n = e.charCodeAt(f++);
+            r = e.charCodeAt(f++);
+            i = e.charCodeAt(f++);
+            s = n >> 2;
+            o = (n & 3) << 4 | r >> 4;
+            u = (r & 15) << 2 | i >> 6;
+            a = i & 63;
+            if (isNaN(r)) {
+              u = a = 64;
+            } else {
+              if (isNaN(i)) {
+                a = 64;
+              }
+            }
+            t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a);
+          }
+          return t;
+        },
+        decode: function(e) {
+          var a, f, i, n, o, r, s, t, u;
+          t = "";
+          n = void 0;
+          r = void 0;
+          i = void 0;
+          s = void 0;
+          o = void 0;
+          u = void 0;
+          a = void 0;
+          f = 0;
+          e = e.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+          while (f < e.length) {
+            s = this._keyStr.indexOf(e.charAt(f++));
+            o = this._keyStr.indexOf(e.charAt(f++));
+            u = this._keyStr.indexOf(e.charAt(f++));
+            a = this._keyStr.indexOf(e.charAt(f++));
+            n = s << 2 | o >> 4;
+            r = (o & 15) << 4 | u >> 2;
+            i = (u & 3) << 6 | a;
+            t = t + String.fromCharCode(n);
+            if (u !== 64) {
+              t = t + String.fromCharCode(r);
+            }
+            if (a !== 64) {
+              t = t + String.fromCharCode(i);
+            }
+          }
+          t = Base64._utf8_decode(t);
+          return t;
+        },
+        _utf8_encode: function(e) {
+          var n, r, t;
+          e = e.replace(/\r\n/g, "\n");
+          t = "";
+          n = 0;
+          while (n < e.length) {
+            r = e.charCodeAt(n);
+            if (r < 128) {
+              t += String.fromCharCode(r);
+            } else if (r > 127 && r < 2048) {
+              t += String.fromCharCode(r >> 6 | 192);
+              t += String.fromCharCode(r & 63 | 128);
+            } else {
+              t += String.fromCharCode(r >> 12 | 224);
+              t += String.fromCharCode(r >> 6 & 63 | 128);
+              t += String.fromCharCode(r & 63 | 128);
+            }
+            n++;
+          }
+          return t;
+        },
+        _utf8_decode: function(e) {
+          var c1, c2, c3, n, r, t;
+          t = "";
+          n = 0;
+          r = c1 = c2 = 0;
+          while (n < e.length) {
+            r = e.charCodeAt(n);
+            if (r < 128) {
+              t += String.fromCharCode(r);
+              n++;
+            } else if (r > 191 && r < 224) {
+              c2 = e.charCodeAt(n + 1);
+              t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+              n += 2;
+            } else {
+              c2 = e.charCodeAt(n + 1);
+              c3 = e.charCodeAt(n + 2);
+              t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+              n += 3;
+            }
+          }
+          return t;
+        }
+      };
+      if (string != null) {
+        return Base64.decode(string);
+      } else {
+        return string;
+      }
+    };
+  });
+
   App.controller("LandCtrl", [
-    "$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase) {
+    "$scope", "$rootScope", "$firebase", "$location", function($scope, $rootScope, $firebase, $location) {
       return $scope.title = "Home";
     }
   ]);
 
   App.controller("UserCtrl", [
-    "$scope", "$rootScope", "$firebase", "GetRepos", "GetCommits", "GetContents", "GetFile", "$base64", function($scope, $rootScope, $firebase, GetRepos, GetCommits, GetContents, GetFile, $base64) {
+    "$scope", "$rootScope", "$firebase", "GetRepos", "GetCommits", "GetContents", "GetFile", function($scope, $rootScope, $firebase, GetRepos, GetCommits, GetContents, GetFile) {
       var secret, trim;
+      $scope.modes = ['Scheme', 'XML', 'Javascript'];
+      $scope.mode = $scope.modes[0];
       $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
         var params;
-        console.log(user.username);
+        console.log(user);
         params = {
-          username: user.username
+          username: user.username,
+          access_token: user.accessToken
         };
         $scope.repos = GetRepos.get(params);
         return console.log($scope.repos);
       });
+      $scope.aceOption = {
+        mode: $scope.mode.toLowerCase(),
+        theme: 'monokai',
+        onLoad: function(_ace) {
+          return $scope.modeChanged = function() {
+            return _ace.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
+          };
+        }
+      };
+      $rootScope.aceModel = "hello";
       $scope.title = "User - loginObj.user";
       $scope.conn = $firebase(new Firebase(URL + "/name"));
       secret = "c2e8b102ca576e0060f6dc493956288170d6eaaa";
       trim = function(x) {
         return x.replace(/^\s+|\s+$/gm, '');
-      };
-      $scope.openItem = function($event) {
-        var params;
-        if ($event.target.children[0] === void 0) {
-          params = {
-            owner: $rootScope.loginObj.user.username,
-            repo: $scope.repos[$scope.repo].name,
-            path: trim($event.target.textContent)
-          };
-          return GetFile.get(params);
-        }
       };
       $scope["new"] = function() {
         return $rootScope.modal = true;
@@ -143,7 +265,8 @@
         if ($rootScope.loginObj.user !== null) {
           params = {
             owner: $rootScope.loginObj.user.username,
-            repo: $scope.repos[$scope.repo].name
+            repo: $scope.repos[$scope.repo].name,
+            access_token: $rootScope.loginObj.user.accessToken
           };
           $scope.repoContents = GetContents.get(params);
           return console.log($scope.repoContents);
@@ -154,7 +277,8 @@
         if ($rootScope.loginObj.user !== null) {
           params = {
             owner: $rootScope.loginObj.user.username,
-            repo: $scope.repos[$scope.repo].name
+            repo: $scope.repos[$scope.repo].name,
+            access_token: $rootScope.loginObj.user.accessToken
           };
           return GetCommits.get(params, function(data) {
             return $scope.sha = data[data.length - 1].sha;
@@ -168,21 +292,13 @@
     function() {
       return {
         restrict: "AE",
-        template: '<li ng-repeat="cont in contents" file="cont" repo="repo"></li>',
+        template: '<li ng-repeat="cont in contents" file="cont" repo="repo" ng-class="{ folder: cont.type == \'dir\' }"></li>',
         scope: {
           folder: "=",
-          repo: '='
+          repo: '=',
+          contents: '='
         },
-        controller: function($rootScope, $scope, GetContents) {
-          var params;
-          console.log($scope.folder);
-          params = {
-            owner: $rootScope.loginObj.user.username,
-            repo: $scope.repo,
-            path: $scope.folder
-          };
-          return $scope.contents = GetContents.get(params);
-        }
+        controller: function($rootScope, $scope, GetContents) {}
       };
     }
   ]);
@@ -191,14 +307,39 @@
     '$compile', function($compile) {
       return {
         restrict: "AE",
-        template: '{{file.name}}',
+        template: '<span ng-click="openItem()">{{file.name}}</span>',
         scope: {
           file: "=",
           repo: "="
         },
+        controller: function($scope, $rootScope, $firebase, GetFile, GetContents, $filter) {
+          return $scope.openItem = function() {
+            var params;
+            params = {
+              owner: $rootScope.loginObj.user.username,
+              repo: $scope.repo,
+              path: $scope.file.path,
+              access_token: $rootScope.loginObj.user.accessToken
+            };
+            if ($scope.file.type !== 'dir') {
+              $rootScope.file = GetFile.get(params);
+              return $rootScope.file.$promise.then(function() {
+                return $rootScope.aceModel = $filter('base64')($rootScope.file.content);
+              });
+            } else {
+              params = {
+                owner: $rootScope.loginObj.user.username,
+                repo: $scope.repo,
+                path: $scope.file.path,
+                access_token: $rootScope.loginObj.user.accessToken
+              };
+              return $scope.contents = GetContents.get(params);
+            }
+          };
+        },
         link: function($scope, $element, attrs) {
           if ($scope.file.type === 'dir') {
-            $element.append('<ul folder="file.path" repo="repo"></ul>');
+            $element.append('<ul folder="file.path" repo="repo" contents="contents"></ul>');
             return $compile($element.contents())($scope);
           }
         }
